5338590cac6a75e0ec79c9b60e59cd92c07d73e2
Andre W. <10945277+alfwro13@users.noreply.github.com>
2025-12-02 09:09:11 +0000

    Update __init__.py for number entities

--- custom_components/ghostfolio/__init__.py
+++ custom_components/ghostfolio/__init__.py
@@ -22,7 +22,7 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.NUMBER]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
